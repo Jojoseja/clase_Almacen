50bd8e8ccf11feaefddb7b2fd5bdb82953767016
jojoseja <jfeyjoo@gmail.com>
2025-12-06 16:11:10 +0100

    first commit

---
+++ src/main/java/com/josefeyjoo/almacen/Clase_Almacen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.josefeyjoo.almacen;
+
+/**
+ *
+ * @author jojo
+ */
+public class Clase_Almacen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
